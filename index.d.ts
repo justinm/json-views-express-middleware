1efc8611bc50a2c50da7eb7175fcd65f5f10ba15
Justin McCormick <justin.mccormick@blackboard.com>
2016-11-07 15:19:11 -0500

    Fixing typescript definitions for express middleware

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,15 @@
+/// <reference path="./node_modules/json-views/index.d.ts" />
+
+declare module "json-views-express-middleware" {
+  export = JsonViewsExpressMiddleware;
+}
+
+declare namespace JsonViewsExpressMiddleware {
+  export function middleware(views: JsonViews.JsonViewsStatic): void;
+}
+
 declare namespace Express {
-  export interface Response {
-    view: (descriptorName: string, data: any) => any;
-  }
+    export interface Response {
+      view: (descriptorName: string, data: any) => any;
+    }
 }
--- index.ts
+++ index.ts
@@ -2,12 +2,6 @@
 import express = require('express');
 import views = require('json-views');
 
-declare namespace Express {
-  export interface Response {
-    view: (descriptorName: string, data: any) => any;
-  }
-}
-
 export function middleware(view: views.JsonViewsStatic) {
   return function(req: express.Request, res: express.Response, next: (err: any) => void) {
     res.view = function (descriptorName: string, data: any) {
