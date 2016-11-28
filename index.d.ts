0d0401a5e4f48250dd6bc3f322f118f7b853d37c
Justin McCormick <justin.mccormick@blackboard.com>
2016-11-28 02:09:43 -0500

    Fixing typescript definitions for express middleware

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,15 @@
 declare module "json-views-express-middleware" {
 
+  import * as jsonViews from 'json-views';
+
   namespace JsonViewsExpressMiddleware {
-    export function middleware(views: JsonViews.JsonViewsStatic): void;
+    export function middleware(views: jsonViews.JsonViewsStatic): void;
   }
 
   export = JsonViewsExpressMiddleware;
 }
 
-declare module "express-serve-static-core" {
+declare namespace Express {
 
   export interface Response {
     view: (descriptorName: string, data: any) => any;
